cc9d610728998dc32dc77d646dfa09ca21727cad
CaptainCorin100 <harrymcgregorwright@gmail.com>
2024-10-29 16:55:28 +0000

    Added image processing functions.

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ def analyse_line_thickness():
     file_path = filedialog.askopenfilename(filetypes=[("JPEG images", "*.jpg")])
     print(file_path)
 
+    img = cv2.imread(file_path)
+    cv2.waitKey(0)
+    grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    th, threshold_img = cv2.threshold(grey_img, 120, 255, cv2.THRESH_OTSU)
+    edges = cv2.Canny(threshold_img, 60, 200)
+    cv2.waitKey(0)
+
+    resized = cv2.resize(edges, (1228, 921))
+    cv2.imshow("Binarised Image", resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+
 if __name__ == "__main__":
     init()
-    
