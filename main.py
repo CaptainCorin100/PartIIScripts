a06b2692b44af79f08576d0bdbf6844a3f7e5b8f
CaptainCorin100 <harrymcgregorwright@gmail.com>
2024-10-31 14:45:19 +0000

    Added centre-line calculation.

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import numpy as np
 kernel_size = (15,15)
 kernel_elem = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
 
+threshold_cutoff = 170
+
+sampling_rate = 2
+
+green = (0,255,0)
+
 def init():
     root = tk.Tk()
     root.title("Part II Scripts")
@@ -27,7 +33,7 @@ def analyse_line_thickness():
     cv2.waitKey(0)
     grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blurred_img = cv2.blur(grey_img, (3,3))
-    th, threshold_img = cv2.threshold(blurred_img, 170, 255, cv2.THRESH_OTSU)
+    th, threshold_img = cv2.threshold(blurred_img, threshold_cutoff, 255, cv2.THRESH_OTSU)
 
     #Process image to extract contours
     closing = cv2.morphologyEx(threshold_img, cv2.MORPH_CLOSE, kernel_elem)
@@ -39,35 +45,41 @@ def analyse_line_thickness():
     sorted_contours = sorted(contours, key=len, reverse=True)
     line_contours = sorted_contours[:2] #Only the largest two contours are actually useful
 
-    contours_poly = [None]*len(contours)
-    boundRect = [None]*len(contours)
-    for i,c in enumerate(contours):
-        contours_poly[i] = cv2.approxPolyDP(c,3,True)
-        boundRect[i] = cv2.boundingRect(contours_poly[i])
+    drawing = img.copy()
 
-    drawing = np.zeros((edges.shape[0], edges.shape[1],3), dtype=np.uint8)
-    for i in range(len(contours)):
-        cv2.drawContours(drawing,contours_poly,i,(0,255,0))
-        cv2.rectangle(drawing, (int(boundRect[i][0]), int(boundRect[i][1])), (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), (0,255,0), 2)
+    def converted_line_points (vx, vy, x, y):
+        lefty = int((-x*vy/vx) + y)
+        righty = int(((img.shape[1]-x)*vy/vx) + y)
+        return [(img.shape[1] - 1, righty), (0, lefty)]
 
-    #h,w = threshold_img.shape[:2]
-    #mask = np.zeros((h+2, w+2), np.uint8)
-    #floodfill_img = threshold_img.copy()
-    #cv2.floodFill(floodfill_img, mask, (0,0), 255)
-    #inverted_img = cv2.bitwise_not(floodfill_img)
-    #combined_img = threshold_img | inverted_img
+    #contours_poly = [None]*len(line_contours)
+    #boundRect = [None]*len(contours)
+    line_points = [None]*len(contours)
 
-    displayed_contours = cv2.drawContours(img,line_contours,-1, (0,255,0), 2, cv2.LINE_AA)
+    for i,c in enumerate(line_contours):
+        #contours_poly[i] = cv2.approxPolyDP(c,100,True)
+        #boundRect[i] = cv2.boundingRect(contours_poly[i])
+        vx, vy, x, y = cv2.fitLine(c, cv2.DIST_L2, 0, 0.01, 0.01)
+        line_points[i] = [vx, vy, x, y]
+        [pt1, pt2] = converted_line_points(vx, vy, x, y)
+        cv2.line (drawing, pt1, pt2, green, 5)
+
+    print([np.arctan2(line_points[0][1], line_points[0][0]),np.arctan2(line_points[1][1], line_points[1][0])])
+    average_gradient = np.tan(np.average([np.arctan2(line_points[0][1], line_points[0][0]),np.arctan2(line_points[1][1], line_points[1][0])]))
+    average_coords_x = np.average([line_points[0][2], line_points[1][2]])
+    average_coords_y = np.average([line_points[0][3], line_points[1][3]])
+    [pt1, pt2] = converted_line_points(1, average_gradient, average_coords_x, average_coords_y)
+    cv2.line (drawing, pt1, pt2, green, 5)
     
-    #dilation = cv2.dilate(edges, kernel, iterations=1)
-    #erosion = cv2.erode(dilation, kernel, iterations=1)
+    cv2.drawContours(drawing, line_contours, -1, green, 2, cv2.LINE_AA)
+    
     cv2.waitKey(0)
 
-    resized = cv2.resize(displayed_contours, (1228, 921))
+    resized = cv2.resize(drawing, (1228, 921))
     cv2.imshow("Binarised Image", resized)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
+
 
 if __name__ == "__main__":
     init()
